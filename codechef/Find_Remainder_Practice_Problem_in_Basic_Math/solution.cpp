ce9603b416ff3885c7f8a124605760f74e31a4ae
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-08-16 13:38:05 +0530

    feat(codechef): add solution for Find_Remainder_Practice_Problem_in_Basic_Math

---
+++ codechef/Find_Remainder_Practice_Problem_in_Basic_Math/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t!=0){
+        int x,y;
+        cin>>x>>y;
+        t--;
+      int result = y%x;
+       cout<<result<<endl;
+      
+    }
+
+}
