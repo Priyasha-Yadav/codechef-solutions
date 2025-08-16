40d7c0fc0857d9bca5f6d4c5a43dba5f7acffa57
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-08-16 13:37:14 +0530

    feat(codechef): add solution for Chef_and_Chocolates_Practice_Problem_in_Basic_Math

---
+++ codechef/Chef_and_Chocolates_Practice_Problem_in_Basic_Math/solution-.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+        // your code goes here
+        cout<< (x*5 + y*10)/ z;
+        cout<<endl;
+    }
+    
+
+}
