654ba7a1acefda08f643b405a5ebf05f4bcc8899
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-08-18 04:26:32 +0530

    feat(codechef): add solution for Sum_of_Digits_Practice_Problem_in_Basic_Math

---
+++ codechef/Sum_of_Digits_Practice_Problem_in_Basic_Math/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int num, sum = 0;
+        cin >> num;
+
+        while (num) {
+            sum += num % 10;
+            num = num / 10;
+        }
+        cout << sum << endl;
+    }
+
+}
