c90ce133ce3db0e2fd30e2a6a25b7469a3020420
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-08-30 21:54:50 +0530

    feat(codechef): add solution for Problems_in_your_todo_list_Practice_Problem_in_500_to_1000_difficulty_rating

---
+++ codechef/Problems_in_your_todo_list_Practice_Problem_in_500_to_1000_difficulty_rating/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        int count = 0;
+        cin >> n;
+        while (n--) {
+            int d;
+            cin >> d;
+            if (d >= 1000) {
+                count += 1;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0;
+
+}
