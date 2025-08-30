f39d8366945b9caf96e8da6c21895ed45650607b
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-08-30 14:17:31 +0530

    feat(codechef): add solution for Car_or_Bike_Practice_Problem_in_500_to_1000_difficulty_rating

---
+++ codechef/Car_or_Bike_Practice_Problem_in_500_to_1000_difficulty_rating/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        if (x == y) {
+            cout << "SAME" << endl;
+        }
+        else if (x > y) {
+            cout << "CAR" << endl;
+        }
+        else {
+            cout << "BIKE" << endl;
+        }
+    }
+
+    return 0;
+
+}
