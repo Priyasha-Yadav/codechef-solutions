0920fac0d0a7b5894ac0fd241eba21a00e50cc38
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-08-30 20:44:04 +0530

    feat(codechef): add solution for The_Three_Topics_Practice_Problem_in_500_to_1000_difficulty_problems

---
+++ codechef/The_Three_Topics_Practice_Problem_in_500_to_1000_difficulty_problems/solution.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, c, x;
+    cin >> a >> b >> c >> x;
+    (a == x || b == x || c == x) ? cout << "YES" << endl: cout << "NO" << endl;
+
+    return 0;
+
+}
