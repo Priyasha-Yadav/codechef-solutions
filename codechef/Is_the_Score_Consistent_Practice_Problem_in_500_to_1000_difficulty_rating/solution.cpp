d82e452d3e665ba04088248be73a2b79034e6cc3
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-08-30 14:31:28 +0530

    feat(codechef): add solution for Is_the_Score_Consistent_Practice_Problem_in_500_to_1000_difficulty_rating

---
+++ codechef/Is_the_Score_Consistent_Practice_Problem_in_500_to_1000_difficulty_rating/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        int c, d;
+        cin >> c >> d;
+        if (a <= c) {
+            (b <= d) ? cout << "POSSIBLE" << endl: cout << "IMPOSSIBLE" << endl;
+
+        }
+        else {
+            cout << "IMPOSSIBLE" << endl;
+        }
+    }
+    return 0;
+}
