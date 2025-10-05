1092f5e0ee95ed455d45e8226c00d0ad7b3497fa
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-10-05 23:41:59 +0530

    feat(codechef): add solution for Data_Types_in_JavaScript_Problem_solving

---
+++ codechef/Data_Types_in_JavaScript_Problem_solving/solution.js
@@ -0,0 +1,12 @@
+// Run the code to view the result
+let a = -5; // Integer variable
+console.log(a);
+
+let b = 4.0; // Float variable
+console.log(b);
+
+let s = "abcd"; // String variable
+console.log(s);
+
+let arr = [7, 8, 9, 10]; // Integer array
+console.log(arr[2]);
